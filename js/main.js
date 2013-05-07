256a072b6d9ecf3ee2aaaeda7dd2e0f0d56e2a49
Shikaga <shikaga@gmail.com>
2013-05-07 21:01:49 +0100

    Added basic timer

--- js/main.js
+++ js/main.js
@@ -16,7 +16,35 @@ function setWordsToDivs(div1, div2) {
 	)
 }
 
-function setWords() {
-	console.log("Winner!");
+function ReadySetArt() {
 	setWordsToDivs(document.getElementById("idea1"), document.getElementById("idea2"));
+	var timer = new Timer();
+	timer.startTimer();
+	var hoursDiv = document.getElementById("hours");
+	var minutesDiv = document.getElementById("minutes");
+	var secondsDiv = document.getElementById("seconds");
+	setInterval(function() {
+		var time = timer.getTimeRemaining();
+		hoursDiv.innerHTML = time.hours;
+		minutesDiv.innerHTML = time.minutes;
+		secondsDiv.innerHTML = time.seconds;
+	}, 50);
+}
+
+var Timer = function() {
+
+	this.timeStarted;
+	this.millisecondsIn90Minutes = 1000 * 60 * 90;
+}
+
+Timer.prototype.startTimer = function() {
+		this.timeStarted = new Date().getTime();
+	}
+
+Timer.prototype.getTimeRemaining = function() {
+	var timeRemaining = this.timeStarted + this.millisecondsIn90Minutes - new Date().getTime();
+	var hours = Math.floor(timeRemaining/1000 / 3600);
+	var minutes = Math.floor((timeRemaining/1000- (hours * 3600)) / 60);
+	var seconds = Math.floor(timeRemaining/1000 % 60);
+	return {hours: hours, minutes: minutes, seconds: seconds};
 }
