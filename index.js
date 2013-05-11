dc9345cacfb3f86a8c52ee3812eb12ad4b5f1387
Jon Paul <shikaga@gmail.com>
2013-05-11 14:25:16 +0100

    Let just get something basic working

--- index.js
+++ index.js
@@ -1,61 +1,75 @@
-//Install node-store: https://github.com/alexkwolfe/node-store
-//var store = require('store')('data');
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-var util = require('util');
-var static = require('node-static');
-//var async = require('async');
-var words = []
-//var io = require('socket.io').listen(8001);
-var millisecondsIn90Minutes = 1000 * 60 * 90;
 
-var webroot = '.';
-fs.readFile('words.txt', function(err, data) {
-	if(err) throw err;
-	words = data.toString().split("\n");
-});
-var file = new(static.Server)(webroot, {
-	cache: 600,
-	headers: { 'X-Powered-By': 'node-static' }
-});
-var server = http.createServer(function (req, res) {
-	req.addListener('end', function() {
-		file.serve(req, res, function(err, result) {
-			if (err) {
-				console.error('Error serving %s - %s', req.url, err.message);
-				if (err.status === 404 || err.status === 500) {
-					file.serveFile(util.format('/%d.html', err.status), err.status, {}, req, res);
-				} else {
-					res.writeHead(err.status, err.headers);
-					res.end();
-				}
-			} else {
-				console.log('%s - %s', req.url, res.message);
-			}
-		});
-	});
+var express = require("express");
+var app = express();
+app.use(express.logger());
+
+app.get('/', function(request, response) {
+	response.send('Hello World!');
 });
 
-function getRandomWords() {
-	return {word1: getRandomWord(), word2: getRandomWord()};
-}
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+	console.log("Listening on " + port);
+});
 
-function getRandomWord() {
-	var word = words[Math.floor((Math.random() * words.length))];
-	var capitalizedWord = toTitleCase(word);
-	return capitalizedWord;
-}
-
-function toTitleCase(str)
-{
-	return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-}
-
-var port = process.env.PORT || 8000;
-server.listen(port);
-
-console.log("Server running at http://127.0.0.1:" + port + "/");
+////Install node-store: https://github.com/alexkwolfe/node-store
+////var store = require('store')('data');
+//var http = require('http');
+//var url = require('url');
+//var fs = require('fs');
+//var util = require('util');
+//var static = require('node-static');
+////var async = require('async');
+//var words = []
+////var io = require('socket.io').listen(8001);
+//var millisecondsIn90Minutes = 1000 * 60 * 90;
+//
+//var webroot = '.';
+//fs.readFile('words.txt', function(err, data) {
+//	if(err) throw err;
+//	words = data.toString().split("\n");
+//});
+//var file = new(static.Server)(webroot, {
+//	cache: 600,
+//	headers: { 'X-Powered-By': 'node-static' }
+//});
+//var server = http.createServer(function (req, res) {
+//	req.addListener('end', function() {
+//		file.serve(req, res, function(err, result) {
+//			if (err) {
+//				console.error('Error serving %s - %s', req.url, err.message);
+//				if (err.status === 404 || err.status === 500) {
+//					file.serveFile(util.format('/%d.html', err.status), err.status, {}, req, res);
+//				} else {
+//					res.writeHead(err.status, err.headers);
+//					res.end();
+//				}
+//			} else {
+//				console.log('%s - %s', req.url, res.message);
+//			}
+//		});
+//	});
+//});
+//
+//function getRandomWords() {
+//	return {word1: getRandomWord(), word2: getRandomWord()};
+//}
+//
+//function getRandomWord() {
+//	var word = words[Math.floor((Math.random() * words.length))];
+//	var capitalizedWord = toTitleCase(word);
+//	return capitalizedWord;
+//}
+//
+//function toTitleCase(str)
+//{
+//	return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+//}
+//
+//var port = process.env.PORT || 8000;
+//server.listen(port);
+//
+//console.log("Server running at http://127.0.0.1:" + port + "/");
 
 
 //io.sockets.on('connection', function (socket) {
