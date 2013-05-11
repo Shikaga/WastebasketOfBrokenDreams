7ee293e7fff924167002cdc32904611b5e9d63cc
Shikaga <shikaga@gmail.com>
2013-05-11 09:19:40 +0100

    Refactor: Reordred main.js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,28 @@
-
-
-function ReadySetArt() {
-	timer.readySetArt();
-}
-
-var timer = new Timer();
-//timer.readySetArt();
-
 var socket = io.connect('http://localhost:8001');
 socket.on('chat', function (data) {
 	console.log(data);
 	lobbyHandler.receiveChatMessage(data);
 });
+socket.on("wordsDrawn", function(data) {
+	console.log(data);
+	var div1 = document.getElementById("idea1");
+	var div2 = document.getElementById("idea2");
+	div1.innerHTML = data.word1;
+	div2.innerHTML = data.word2;
+});
+
+var timer = new Timer();
+var lobbyHandler = new LobbyHandler();
+
+lobbyHandler.joinLobbyFromUrl();
+
+function chatButtonClicked() {
+	lobbyHandler.chatButtonClicked();
+}
+
+function createLobby() {
+	lobbyHandler.createLobby();
+}
 
 function getWords() {
 	console.log("lobbyId", lobbyHandler.lobbyId);
@@ -23,22 +34,6 @@ function getWords() {
 	}
 }
 
-
-socket.on("wordsDrawn", function(data) {
-	console.log(data);
-	var div1 = document.getElementById("idea1");
-	var div2 = document.getElementById("idea2");
-	div1.innerHTML = data.word1;
-	div2.innerHTML = data.word2;
-});
-
-var lobbyHandler = new LobbyHandler();
-lobbyHandler.joinLobbyFromUrl();
-
-function chatButtonClicked() {
-	lobbyHandler.chatButtonClicked();
-}
-
-function createLobby() {
-	lobbyHandler.createLobby();
+function ReadySetArt() {
+	timer.readySetArt();
 }
