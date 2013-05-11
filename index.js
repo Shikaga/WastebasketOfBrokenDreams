2375f3264697bf865f529cc2e23b4b6b59e4de39
Jon Paul <shikaga@gmail.com>
2013-05-11 14:20:49 +0100

    Commenting out all socket io stuff for a second

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var util = require('util');
 var static = require('node-static');
 //var async = require('async');
 var words = []
-var io = require('socket.io').listen(8001);
+//var io = require('socket.io').listen(8001);
 var millisecondsIn90Minutes = 1000 * 60 * 90;
 
 var webroot = '.';
@@ -58,78 +58,78 @@ server.listen(port);
 console.log("Server running at http://127.0.0.1:" + port + "/");
 
 
-io.sockets.on('connection', function (socket) {
-	socket.on('chat', function (data) {
-		sendChatMessages(data)
-	});
-	socket.on('createLobby', function() {
-		var data = createLobby();
-		socket.emit("lobbyCreated", data);
-	})
-	socket.on("loginAdmin", function(data) {
-		console.log("Login Admin", data);
-		var lobby = lobbies[data.lobbyId];
-		if (lobby != null) {
-			lobby.connectedSockets.push(socket);
-			sendWords(socket,lobby);
-		}
-	});
-	socket.on("loginGuest", function(data) {
-		var lobby = lobbies[data.lobbyId];
-		if (lobby != null) {
-			lobby.connectedSockets.push(socket);
-			sendWords(socket,lobby);
-		}
-	});
-	socket.on("drawWords", function(data) {
-		console.log("draw", data);
-		var randomWords = getRandomWords();
-		var timeNow = new Date().getTime();
-		var endTime = timeNow + millisecondsIn90Minutes
-		if (data) {
-			var lobby = lobbies[data.lobbyId];
-			if (lobby != null) {
-				lobby.word1 = randomWords.word1;
-				lobby.word2 = randomWords.word2;
-				lobby.endTime = endTime;
-				for (var i=0; i < lobby.connectedSockets.length; i++) {
-					sendWords(lobby.connectedSockets[i],lobby);
-				}
-			}
-		} else {
-			var timeRemaining = endTime - timeNow;
-			var returnMessage = randomWords;
-			returnMessage["timeRemaining"]=timeRemaining;
-			socket.emit("wordsDrawn", returnMessage);
-		}
-	});
-});
-
-function sendWords(socket, lobby) {
-	console.log("SEND WORDS", lobby);
-	if (lobby.word1 != null) {
-		console.log(0);
-		socket.emit("wordsDrawn",
-			{word1:lobby.word1, word2:lobby.word2, timeRemaining: lobby.endTime - new Date().getTime()});
-	}
-}
-
-var lobbies = [];
-
-function sendChatMessages(data) {
-	console.log(data);
-	console.log(data.lobby);
-	var lobby = lobbies[data.lobby];
-	for (var i=0; i < lobby.connectedSockets.length; i++) {
-		lobby.connectedSockets[i].emit("chat", data);
-	}
-}
-
-
-function createLobby() {
-	var password = Math.random().toString().substring(2);
-	lobbies.push({password: password, connectedSockets:[]});
-	return {lobbyId: lobbies.length-1, password: password};
-}
-
-//Use node rooms
+//io.sockets.on('connection', function (socket) {
+//	socket.on('chat', function (data) {
+//		sendChatMessages(data)
+//	});
+//	socket.on('createLobby', function() {
+//		var data = createLobby();
+//		socket.emit("lobbyCreated", data);
+//	})
+//	socket.on("loginAdmin", function(data) {
+//		console.log("Login Admin", data);
+//		var lobby = lobbies[data.lobbyId];
+//		if (lobby != null) {
+//			lobby.connectedSockets.push(socket);
+//			sendWords(socket,lobby);
+//		}
+//	});
+//	socket.on("loginGuest", function(data) {
+//		var lobby = lobbies[data.lobbyId];
+//		if (lobby != null) {
+//			lobby.connectedSockets.push(socket);
+//			sendWords(socket,lobby);
+//		}
+//	});
+//	socket.on("drawWords", function(data) {
+//		console.log("draw", data);
+//		var randomWords = getRandomWords();
+//		var timeNow = new Date().getTime();
+//		var endTime = timeNow + millisecondsIn90Minutes
+//		if (data) {
+//			var lobby = lobbies[data.lobbyId];
+//			if (lobby != null) {
+//				lobby.word1 = randomWords.word1;
+//				lobby.word2 = randomWords.word2;
+//				lobby.endTime = endTime;
+//				for (var i=0; i < lobby.connectedSockets.length; i++) {
+//					sendWords(lobby.connectedSockets[i],lobby);
+//				}
+//			}
+//		} else {
+//			var timeRemaining = endTime - timeNow;
+//			var returnMessage = randomWords;
+//			returnMessage["timeRemaining"]=timeRemaining;
+//			socket.emit("wordsDrawn", returnMessage);
+//		}
+//	});
+//});
+//
+//function sendWords(socket, lobby) {
+//	console.log("SEND WORDS", lobby);
+//	if (lobby.word1 != null) {
+//		console.log(0);
+//		socket.emit("wordsDrawn",
+//			{word1:lobby.word1, word2:lobby.word2, timeRemaining: lobby.endTime - new Date().getTime()});
+//	}
+//}
+//
+//var lobbies = [];
+//
+//function sendChatMessages(data) {
+//	console.log(data);
+//	console.log(data.lobby);
+//	var lobby = lobbies[data.lobby];
+//	for (var i=0; i < lobby.connectedSockets.length; i++) {
+//		lobby.connectedSockets[i].emit("chat", data);
+//	}
+//}
+//
+//
+//function createLobby() {
+//	var password = Math.random().toString().substring(2);
+//	lobbies.push({password: password, connectedSockets:[]});
+//	return {lobbyId: lobbies.length-1, password: password};
+//}
+//
+////Use node rooms
